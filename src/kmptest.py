d15fc1265bde1cc36a5629b37df4560c8d29e4b6
jeffreydrew <98627285+jeffreydrew@users.noreply.github.com>
2022-12-05 20:03:50 -0500

    ,,,

---
+++ src/kmptest.py
@@ -0,0 +1,55 @@
+def kmp(pattern, text):
+    a = len(text)
+    b = len(pattern)
+
+    prefix_arr = get_prefix_arr(pattern, b)
+
+    initial_point = []
+
+    m = 0
+    n = 0
+
+    while m != a:
+
+        if text[m] == pattern[n]:
+            m += 1
+            n += 1
+
+        else:
+            n = prefix_arr[n - 1]
+
+        if n == b:
+            initial_point.append(m - n)
+            n = prefix_arr[n - 1]
+        elif n == 0:
+            m += 1
+
+    return initial_point
+
+
+def lps(pattern, b):
+    lps = [0] * b
+    n = 0
+    m = 1
+
+    while m != b:
+        if pattern[m] == pattern[n]:
+            n += 1
+            lps[m] = n
+            m += 1
+        elif n != 0:
+            n = lps[n - 1]
+        else:
+            lps[m] = 0
+            m += 1
+
+    return lps
+
+
+string = "abcabcabc"
+pat = "abc"
+
+initial_index = kmp(pat, string)
+
+for i in initial_index:
+    print("Pattern is found in the string at index number", i)
